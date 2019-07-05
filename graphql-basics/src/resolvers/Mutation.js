7fdca875dda0e1bd3d6a4cd0ee4861faf3396c81
Eder Ramirez <eder.ramirez87@gmail.com>
2019-07-05 12:06:36 -0500

    updating posts and comments

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 import uuidv4 from 'uuid/v4';
+import { type } from 'os';
 
 const Mutation = {
   createUser(parent, args, { db }, info) {
@@ -100,6 +101,28 @@ const Mutation = {
     return deletedPost[0];
 
   },
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+    const post = db.posts.find(post => post.id === id);
+
+    if (!post) {
+      throw new Error("Post does not exists!");
+    }
+
+    if (typeof data.title === "string" ) {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === "string" ) {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === "boolean" ) {
+      post.published = data.published;
+    }
+
+    return post;
+  },
   createComment(parent, args, { db }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
     const postExists = db.posts.some(el => el.id === args.data.post && el.published);
@@ -129,6 +152,20 @@ const Mutation = {
 
     return deletedComment[0];
 
+  },
+  updateComment(parent, args, { db }, info){
+    const{ id, data } = args;
+    const comment = db.comments.find(comment => comment.id === id);
+
+    if (!comment) {
+      throw new Error("Comment not found!");
+    }
+
+    if (typeof data.text === "string") {
+      comment.text = data.text
+    }
+
+    return comment;
   }
 };
 
