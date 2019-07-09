bc26d8a39733e2c77ea13ada4908736c02beb2f1
Eder Ramirez <eder.ramirez87@gmail.com>
2019-07-09 09:53:23 -0500

    Using async await with prisma bindings

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -10,47 +10,48 @@ const prisma = new Prisma({
 // prisma.subscription
 // prisma.exists
 
-/* prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-  console.log("TCL: data", JSON.stringify(data, undefined, 2));
-}) */
-
-/* prisma.query.comments(null, '{ id text author { id name } }').then(data => {
-  console.log("TCL: data", JSON.stringify(data, undefined, 2))
-}). catch(err => {
-  console.log("TCL: err", err)
-}); */
-
-/* prisma.mutation.createPost({
-  data: {
-    title: "GraphQL 101",
-    body: "",
-    published: false,
-    author: {
-      connect: {
-        id: "cjxusgotc00910802oehc4so2"
+const createPostForUser = async (authorId, data) => {
+  const post = await prisma.mutation.createPost({
+    data: {
+      ...data,
+      author: {
+        connect: {
+          id: authorId
+        }
       }
     }
-  }
-}, '{ id title body published }').then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
-  return prisma.query.users(null, '{ id name posts { id title } }')
-}).then((data) => {
-  console.log(JSON.stringify(data, undefined, 2));
-}); */
+  }, '{ id }');
 
-prisma.mutation.updatePost({
-  data: {
-    published: true
-  },
-  where: {
-    id: "cjxuwjq4j01bl080257mcv04b"
-  }
-}, '{ id title body published }').then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
+  const user = await prisma.query.user({
+    where: {
+      id: authorId
+    }
+  }, '{ id name email posts { id title published } }');
 
-  return prisma.query.posts(null, '{ id title body published }')
-}).then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
-}).catch(err => {
-  console.log(err);
-});
+  return user;
+};
+
+const updatePostForUser = async (postId, data) => {
+  const post = await prisma.mutation.updatePost({
+    where: {
+      id: postId
+    },
+    data
+  }, '{ author {id} }' )
+
+  const authorId = post.author.id;
+  const user = await prisma.query.user({
+    where: {
+      id: authorId
+    }
+  }, '{ id name email posts { id title published } }');
+
+  return user;
+}
+
+/* updatePostForUser("cjxvwszwo01e50802i185zd0c", {
+  published: false
+}).then(user => {
+console.log("TCL: user", JSON.stringify( user, undefined, 2))
+  
+}). catch(err => console.log(err)); */
